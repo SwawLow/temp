af6296111a2c5ca097bad5597377a7d75929c1cd
SwawLow <sebastian.dsheridan.it@gmail.com>
2025-10-15 00:27:12 +0200

    Update bioinfo.py

--- bioinfo.py
+++ bioinfo.py
@@ -21,6 +21,12 @@ def positions(s: str, p: str) -> list:
     post: returns the indexes of the where starts the objective  (ex: find p(cg) in s(ACCGACCG) -> [1,5])
     
     """
+    tempListCluster = [s,p]
+    tempListClusterLength = len(tempListCluster)
+
+    for j in range(tempListClusterLength):
+        if not is_adn(tempListCluster[j]):
+            return f"{tempListCluster[j]} is not a valid dna sequence"
 
     s = s.upper()
     p = p.upper()
@@ -49,6 +55,13 @@ def distance_h(s1: str, s2: str) -> int:
     post: returns the number of differences in each list (ex: compare s1(ACG) and s2(ACT) -> 1)
     
     """
+
+    tempListCluster = [s1,s2]
+    tempListClusterLength = len(tempListCluster)
+
+    for j in range(tempListClusterLength):
+        if not is_adn(tempListCluster[j]):
+            return f"{tempListCluster[j]} is not a valid dna sequence"
 
     s1.upper()
     s2.upper()
@@ -96,10 +109,32 @@ def distances_matrice(l: str|list):
     return tempList
 
 
-#["AG", "AT", "GT", "ACG", "ACT" ]
-#"AT"
+print("\nFonction — is adn:")
+print(f"{is_adn('AC')} #A et C font partie des 4 caracter autoriser(A,C,G & T)")
+print(f"{is_adn('AS')} >>> AS is not a valid dna sequence #S ne peut etre dans l'input")
+#is_adn
 
+print("\nFonction — positions:")
+print(f"{positions('ACGTACTCAC','AS')} >>> AS is not a valid dna sequence #S ne peut etre dans l'input")
+print(f"{positions('ACGTACTCAC','AC')} #a indexe 0, 4 et 8 commence la combinaisons des caracter AC")
+#positions
+
+print("\nFonction — distances_h:")
+print(f"{distance_h('GACGTAGCTCAC','AGATGTACGACT')} #Est le nombre de distance Hamming")
+print(f"{distance_h('GACGTAGCTCACSSS','AGATGTACGACT')} #S ne peut etre dans l'input")
+print(f"{distance_h('GACGTAGCTCACTTT','AGATGTACGACT')} #La longeur du premier input est different de la deuxieme")
+#distances_h
+
+print("\nFonction — distances_matrice:\n")
 fnshdProd = distances_matrice(["AG", "AT", "GT", "ACG", "ACT" ])
-
 for t in range(len(fnshdProd)):
     print(fnshdProd[t])
+
+fnshdProd = distances_matrice("AT")
+print("#Ci-dessus represent la matrice de la list ['AG', 'AT', 'GT', 'ACG', 'ACT']\nqui a ete traité par la fonction distances_h()\n")
+for t in range(len(fnshdProd)):
+    print(fnshdProd[t])
+print("#Ci-dessus represent la matrice de 'A' et 'T' qui a ete traité par la fonction distances_h()\n")
+
+print(f"{distances_matrice('AS')} #S ne peut etre dans l'input")
+#distances_matrice
